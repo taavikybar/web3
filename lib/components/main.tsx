28c8ee2f3fe46456c83adea449e7b93d106ff249
Taavi Kybar <taavi.kubar@pipedrive.com>
2022-10-07 12:25:05 +0300

    remove api key

--- lib/components/main.tsx
+++ lib/components/main.tsx
@@ -7,7 +7,6 @@ import convertLibAbi from '../../build/abi/ConvertLib.json'
 
 const metaCoinAddress = '0x03BCce64A27d64B2d1bFD9FE6e2164fc3Abd0057'
 const convertLibAddress = '0x2De112030a939DaA04e5030CDA195337eC6C2E34'
-// const apiKey = '68f896a78c8749b4adc3e7e61a0ef05c'
 // const provider = `https://mainnet.infura.io/v3/${apiKey}`
 const provider = 'ws://localhost:7545'
 const address = '0xb28d290EC228EDfe48Ab61AeAcE78285B802a774'
